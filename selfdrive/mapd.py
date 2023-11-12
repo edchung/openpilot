04aa6fa25427ad353648eff61d7c8c5e9fb54f9f
Jason Wen <haibin.wen3@gmail.com>
2023-11-12 13:31:20 -0500

    sunnypilot v2023.11.12 version: sunnypilot v0.9.5.1 release date: 2023-11-12T13:34:15 master commit: 4e0df9058baa06a107b54eb7a79cb9ead2886b69

---
+++ selfdrive/mapd.py
@@ -0,0 +1,76 @@
+# PFEIFER - MAPD
+import glob
+import os
+import shutil
+import subprocess
+import urllib.request
+
+from openpilot.common.realtime import Ratekeeper
+import stat
+
+VERSION = 'v1.5.6'
+URL = f"https://github.com/pfeiferj/openpilot-mapd/releases/download/{VERSION}/mapd"
+MAPD_PATH = '/data/media/0/osm/mapd'
+VERSION_PATH = '/data/media/0/osm/mapd_version'
+
+
+def download():
+  with urllib.request.urlopen(URL) as f:
+    with open(MAPD_PATH, 'wb') as output:
+      output.write(f.read())
+      os.fsync(output)
+      current_permissions = stat.S_IMODE(os.lstat(MAPD_PATH).st_mode)
+      os.chmod(MAPD_PATH, current_permissions | stat.S_IEXEC)
+    with open(VERSION_PATH, 'w') as output:
+      output.write(VERSION)
+      os.fsync(output)
+
+
+def cleanup_OSM_data():
+  paths = [
+    "/data/media/0/osm/db",
+    "/data/media/0/osm/v*"
+  ]
+  for path in paths:
+    files = glob.glob(path + '/**', recursive=True)
+    for file in files:
+      try:
+        shutil.rmtree(file, ignore_errors=True)
+      except NotADirectoryError:
+        os.remove(file)
+  for path in paths:
+    shutil.rmtree(path, ignore_errors=True)
+
+
+def mapd_thread():
+  rk = Ratekeeper(0.05, print_delay_threshold=None)
+  cleanup_OSM_data()
+
+  while True:
+    try:
+      if not os.path.exists(MAPD_PATH):
+        download()
+        continue
+      if not os.path.exists(VERSION_PATH):
+        download()
+        continue
+      with open(VERSION_PATH) as f:
+        content = f.read()
+        if content != VERSION:
+          download()
+          continue
+
+      process = subprocess.Popen(MAPD_PATH, stdout=subprocess.PIPE)
+      process.wait()
+    except Exception as e:
+      print(e)
+
+    rk.keep_time()
+
+
+def main():
+  mapd_thread()
+
+
+if __name__ == "__main__":
+  main()
